c868c1fc8c734c913ab8c709ad6d894a9a960f94
Matthew Zita <matthew.zita@atco.com>
2022-09-07 12:18:43 -0600

    completed day08

--- 08/main.py
+++ 08/main.py
@@ -18,22 +18,76 @@ def readFile(fileName):
 def part1(lines):
     # Code the solution to part 1 here, returning the answer as a string
     
-    
+    WIDTH = 25
+    HEIGHT = 6
+    DEPTH = len(lines[0]) // (WIDTH*HEIGHT)
 
-    return(f"ANSWER HERE") 
+    img = [[[0 for i in range(WIDTH)] for j in range(HEIGHT)] for k in range(DEPTH)]
+
+    for i, pixel in enumerate(lines[0]):
+        layer_i = i // (WIDTH*HEIGHT)
+        row_i = (i%(WIDTH*HEIGHT)) // WIDTH
+        col_i = (i%WIDTH)
+
+        img[layer_i][row_i][col_i] = int(pixel)
+
+    fewest0 = WIDTH*HEIGHT*DEPTH
+    fewest1 = 0
+    fewest2 = 0
+
+    for layer in img:
+        flat = [pixel for line in layer for pixel in line]
+        count0 = flat.count(0)
+        if count0 < fewest0:
+            fewest0 = count0
+            fewest1 = flat.count(1)
+            fewest2 = flat.count(2)
+
+    return(f"The product of 1s and 2s in the layer with the least 0s is {fewest1*fewest2}") 
 
 def part2(lines):
     # Code the solution to part 2 here, returning the answer as a string
     
-    
+    WIDTH = 25
+    HEIGHT = 6
+    DEPTH = len(lines[0]) // (WIDTH*HEIGHT)
 
-    pass
+    img = [[[0 for i in range(WIDTH)] for j in range(HEIGHT)] for k in range(DEPTH)]
+
+    for i, pixel in enumerate(lines[0]):
+        layer_i = i // (WIDTH*HEIGHT)
+        row_i = (i%(WIDTH*HEIGHT)) // WIDTH
+        col_i = (i%WIDTH)
+
+        img[layer_i][row_i][col_i] = int(pixel)
+
+
+    finalimg = [[0 for i in range(WIDTH)] for j in range(HEIGHT)]
+    for row in range(HEIGHT):
+        for col in range(WIDTH):
+            layer = 0
+            while img[layer][row][col] == 2:
+                layer += 1
+            finalimg[row][col] = img[layer][row][col]
+
+
+    for row in finalimg:
+        msg = ""
+        for pixel in row:
+            if pixel == 1:
+                msg = msg + "▮"
+            else:
+                msg = msg + " "
+        print(msg)
+
+    return(f"Read the text above")
 
 def main ():
     # Opens a dialog to select the input file
     # Times and runs both solutions
     # Prints the results
-    fileName = easygui.fileopenbox(default=f"./"+AOCDAY+"/"+"*.txt")
+    # fileName = easygui.fileopenbox(default=f"./"+AOCDAY+"/"+"*.txt")
+    fileName = "//atco/homedrive/cgy2/X9VR$/My Documents/GitHub/aoc2019py-team-fork/08/matthew-input.txt"
     if fileName == None:
         print("ERROR: No file selected.")
         return
