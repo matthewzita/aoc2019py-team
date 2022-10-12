b9bc774c3eda0e92ef3be8e4b77c219c21f84ecd
Matthew Zita <matthew.zita@atco.com>
2022-10-12 16:05:55 -0600

    copy pasted day 11 and 12

--- 11/main.py
+++ 11/main.py
@@ -1,9 +1,5 @@
 import easygui
 import time
-import itertools
-import math
-
-
 
 AOCDAY = "11"
 
@@ -218,83 +214,214 @@ class ComputerState:
         opCode = self.memory[self.programCounter]
         return outputStream,opCode
 
-class Pixel:
+class RunResult:
+    def __init__(self, outputStream, opCode):
+        self.outputStream = outputStream
+        self.opCode = opCode
 
-    def __init__(self, x, y, colour=0):
+class ComputerState2:
+    def __init__(self, programString):
+        self.memory = [int(number) for number in programString.split(",")]
+        self.programCounter = 0
+        self.offset = 0
+
+    def memCheck(self, address):
+        while len(self.memory) <= address:
+            self.memory.append(0)
+
+    def run(self,inputStream):
+        outputStream = []
+
+        INSTRUCTIONLENGTHS = {
+            '1': 4,
+            '2': 4,
+            '3': 2,
+            '4': 2,
+            '5': 3,
+            '6': 3,
+            '7': 4,
+            '8': 4,
+            '9': 2
+        }
+
+        opCode = self.memory[self.programCounter] % 100
+        while self.memory[self.programCounter] != 99:
+
+            opCode = self.memory[self.programCounter] % 100
+            paramA = self.memory[self.programCounter] % 1000 // 100
+            paramB = self.memory[self.programCounter] % 10000 // 1000
+            paramC = self.memory[self.programCounter] % 100000 // 10000
+
+            addressA = 0
+            if paramA == 0:
+                addressA = self.memory[self.programCounter+1]
+            elif paramA == 1:
+                addressA = self.programCounter+1
+            elif paramA == 2:
+                addressA = self.memory[self.programCounter+1] + self.offset
+            self.memCheck(addressA)
+
+            if(INSTRUCTIONLENGTHS[str(opCode)] >= 3):
+                addressB = 0
+                if paramB == 0:
+                    addressB = self.memory[self.programCounter+2]
+                elif paramB == 1:
+                    addressB = self.programCounter+2
+                elif paramB == 2:
+                    addressB = self.memory[self.programCounter+2] + self.offset
+                self.memCheck(addressB)
+
+            if(INSTRUCTIONLENGTHS[str(opCode)] >= 4):
+                addressC = 0
+                if paramC == 0:
+                    addressC = self.memory[self.programCounter+3]
+                elif paramC == 1:
+                    addressC = self.programCounter+3
+                elif paramC == 2:
+                    addressC = self.memory[self.programCounter+3] + self.offset
+                self.memCheck(addressC)
+
+            # print(self.programCounter, opCode, paramA, paramB, paramC, addressA, addressB, addressC)
+            # print(self.memory)
+
+            if opCode == 1:
+                self.memory[addressC] = self.memory[addressA] + self.memory[addressB]
+
+            elif opCode == 2:
+                self.memory[addressC] = self.memory[addressA] * self.memory[addressB]
+
+            elif opCode == 3:
+                if len(inputStream) < 1:
+                    return RunResult(outputStream,opCode)
+                self.memory[addressA] = inputStream.pop(0)
+
+            elif opCode == 4:
+                outputStream.append(self.memory[addressA])
+
+            elif opCode == 5:
+                if self.memory[addressA] != 0:
+                    self.programCounter = self.memory[addressB] - 3
+
+            elif opCode == 6:
+                if self.memory[addressA] == 0:
+                    self.programCounter = self.memory[addressB] - 3
+
+            elif opCode == 7:
+                if self.memory[addressA] < self.memory[addressB]:
+                    self.memory[addressC] = 1
+                else:
+                    self.memory[addressC] = 0
+
+            elif opCode == 8:
+                if self.memory[addressA] == self.memory[addressB]:
+                    self.memory[addressC] = 1
+                else:
+                    self.memory[addressC] = 0
+
+            elif opCode == 9:
+                self.offset += self.memory[addressA]
+
+            else:
+                print("Error: Unknown opcode")
+                break
+
+            self.programCounter += INSTRUCTIONLENGTHS[str(opCode)]
+
+        opCode = self.memory[self.programCounter] % 100
+        return RunResult(outputStream,opCode)
+
+class Pixel:
+    def __init__(self,x,y,colour=0):
         self.x = x
         self.y = y
         self.colour = colour
-
     def __str__(self):
-        return f"({self.x}, {self.y})"
-
+        return f"({self.x},{self.y})"
     def __repr__(self):
-        return f"{self.colour}: ({self.x}, {self.y})"
-
-    def __eq__(self, other):
-        return (self.x == other.y) and (self.y == other.y)
-
-    def __add__(self, other):
-        return Pixel(self.x + other.x, self.y + other.y, self.colour)
-
-def checkIfDuplicates_1(listOfElems):
-    ''' Check if given list contains any duplicates '''
-    if len(listOfElems) == len(set(listOfElems)):
-        return False
-    else:
-        return True
+        return f"({self.x},{self.y})"
+    def __eq__(self,other):
+        return self.x == other.x and self.y == other.y
+    def __add__(self,other):
+        return Pixel(self.x + other.x, self.y+other.y, self.colour)
 
 def part1(lines):
     # Code the solution to part 1 here, returning the answer as a string
-    
-    computer = ComputerState(lines[0])
-
-    painted_pixels = []
-    computer_state = 3  # set initial state to waiting for input
-    current_pixel = Pixel(0, 0) # start at the origin
-    directions = [Pixel(0, 1), Pixel(1, 0), Pixel(0, -1), Pixel(-1, 0)] # up, right, down, left
-    current_direction = 0   # point up at the start
-
-    # while computer_state == 3:
-    for i in range(100):
-        output, computer_state = computer.run([current_pixel.colour])
-
-        current_pixel.colour = output[0]
-
-        if current_pixel not in painted_pixels:
-            painted_pixels.append(current_pixel)
-
+    computer = ComputerState2(lines[0])
+    # output = computer.run([0])
+    paintedPixels = []
+    computerState = 3
+    currentPixel = Pixel(0,0,0)
+    directions = [Pixel(0,1),Pixel(1,0),Pixel(0,-1),Pixel(-1,0)]
+    currentDirection = 0
+    while computerState == 3:
+    # for i in range(10):
+        result = computer.run([currentPixel.colour])
+        output = result.outputStream
+        computerState = result.opCode
+        currentPixel.colour = output[0]
+        if currentPixel not in paintedPixels:
+            paintedPixels.append(currentPixel)
         if output[1] == 1:
-            current_direction = (current_direction + 1) % 4
+            currentDirection = (currentDirection + 1) % 4
         else:
-            current_direction = (current_direction - 1) % 4
-
-        new_pixel = current_pixel + directions[current_direction]
-
-        # print(f"new pixel: {new_pixel} colour: {current_pixel.colour}")
-
-        if new_pixel not in painted_pixels:
-            current_pixel = new_pixel
+            currentDirection = (currentDirection - 1) % 4
+        newPixel = currentPixel + directions[currentDirection]
+        if newPixel not in paintedPixels:
+            currentPixel = newPixel
+            newPixel.colour = 0
         else:
-            current_pixel = painted_pixels[painted_pixels.index(new_pixel)]
-    
-    if checkIfDuplicates_1(painted_pixels):
-        print("yes")
-    else:
-        print("no")
-
-    return(f"The number of panels coloured at least once is {len(painted_pixels)}")
-
+            currentPixel = paintedPixels[paintedPixels.index(newPixel)]
+    return(f"The number of painted pixels is {len(paintedPixels)}")
 
 def part2(lines):
-    # Code the solution to part 1 here, returning the answer as a string
+    # Code the solution to part 2 here, returning the answer as a string
+    computer = ComputerState2(lines[0])
+    paintedPixels = []
+    computerState = 3
+    currentPixel = Pixel(0,0,1)
+    directions = [Pixel(0,1),Pixel(1,0),Pixel(0,-1),Pixel(-1,0)]
+    currentDirection = 0
+    while computerState == 3:
+        result = computer.run([currentPixel.colour])
+        output = result.outputStream
+        computerState = result.opCode
+        currentPixel.colour = output[0]
+        if currentPixel not in paintedPixels:
+            paintedPixels.append(currentPixel)
+        if output[1] == 1:
+            currentDirection = (currentDirection + 1) % 4
+        else:
+            currentDirection = (currentDirection - 1) % 4
+        newPixel = currentPixel + directions[currentDirection]
+        if newPixel not in paintedPixels:
+            currentPixel = newPixel
+            newPixel.colour = 0
+        else:
+            currentPixel = paintedPixels[paintedPixels.index(newPixel)]
+    whitePixels = []
+    for pixel in paintedPixels:
+        if pixel.colour == 1:
+            whitePixels.append(pixel)
+    minX = 0
+    maxX = 0
+    minY = 0
+    maxY = 0
+    for pixel in whitePixels:
+        minX = min(minX, pixel.x)
+        maxX = max(maxX, pixel.x)
+        minY = min(minY, pixel.y)
+        maxY = max(maxY, pixel.y)
 
-    pass
-    # computer = ComputerState(lines[0])
+    for y in range(maxY, minY - 1, -1):
+        printLine = ""
+        for x in range(minX, maxX+1, 1):
+            if Pixel(x, y, 0) in whitePixels:
+                printLine += "██"
+            else:
+                printLine += "  "
+        print(printLine)
 
-    # output = computer.run([2])[0][0]
-    
-    # return(f"The coordinates of the distress signal are {output}")
+    return(f"Read the code above.")
 
 def main ():
     # Opens a dialog to select the input file
